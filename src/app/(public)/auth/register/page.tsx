edce7f8daeb9212c91738699a5f1c72ac525f9f1
taehyeon <taehyeon@elicer.com>
2025-05-09 19:24:28 +0900

    refactor: update .gitignore to include .env files.

--- src/app/(public)/auth/register/page.tsx
+++
@@ -1,5 +0,0 @@
-const RegisterPage: React.FC = () => {
-  return <div>RegisterPage</div>;
-};
-
-export default RegisterPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,4 @@
-'use client';
-
-import { useEffect } from 'react';
-
 const Home: React.FC = () => {
-  useEffect(() => {
-    setTimeout(() => {
-      throw new Error('test');
-    }, 1000);
-  }, []);
-
   return <div>Home</div>;
 };
 
