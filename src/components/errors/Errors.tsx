f204f652aef4b9ee15e03c33a3d7b245c6398325
taehyeon <taehyeon@elicer.com>
2025-05-11 19:22:36 +0900

    chore: update dependencies and add new error handling image

--- src/components/errors/Errors.tsx
+++ src/components/errors/Errors.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Image from 'next/image';
+
+import { Button } from '../ui/button';
 
 import type { FallbackProps } from 'react-error-boundary';
+
+import { cn } from '@/lib/utils';
 
 //
 //
@@ -13,10 +18,20 @@ type ErrorsProps = FallbackProps & React.ComponentProps<'div'>;
 //
 
 const Errors = React.forwardRef<HTMLDivElement, ErrorsProps>(
-  ({ ...props }, ref) => {
+  ({ resetErrorBoundary, ...props }, ref) => {
     return (
-      <div {...props} ref={ref}>
-        error
+      <div
+        {...props}
+        ref={ref}
+        className={cn('flex h-dvh flex-col items-center justify-center gap-4')}
+      >
+        <div className={cn('relative aspect-square w-full max-w-96')}>
+          <Image src="/images/errors/server-down.svg" alt="error-image" fill />
+        </div>
+        <p className={cn('text-center text-2xl font-bold')}>
+          죄송합니다. 예기치 못한 오류가 발생했어요. 잠시 후 다시 시도해주세요.
+        </p>
+        <Button onClick={resetErrorBoundary}>홈으로 돌아가기</Button>
       </div>
     );
   }
